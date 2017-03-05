f5d5ca95d3eaa07898cd71e4cfeedf72d96b1fa2
iOS <trn15@trn15s-MacBook-Pro.local>
2017-03-05 14:24:33 +0300

    Zainab

---
+++ MHealth/EntryViewController.swift
@@ -0,0 +1,41 @@
+//
+//  EntryViewController.swift
+//  MHealth
+//
+//  Created by trn2 on 3/5/17.
+//  Copyright © 2017 PIFSS. All rights reserved.
+//
+
+import UIKit
+
+class EntryViewController: UIViewController {
+
+    @IBAction func ArabicButton(sender: AnyObject) {
+    }
+    
+    @IBAction func EnglishButton(sender: AnyObject) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
