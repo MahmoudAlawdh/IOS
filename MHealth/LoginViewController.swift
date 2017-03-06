db46be95ff403f1ad603c23628dce6187d1d5818
iOS <trn24@trn24s-MacBook-Pro.local>
2017-03-06 12:13:46 +0300

    Shaikha's login updated

--- MHealth/LoginViewController.swift
+++ MHealth/LoginViewController.swift
@@ -8,12 +8,50 @@
 
 import UIKit
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController , NetworkCaller {
 
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var username: UITextField!
+    @IBAction func donorLogin(sender: AnyObject) {
+    
+        self.networkManager.AMGetArrayData("http://34.196.107.188:8080/mHealthWS/ws/donor", params: [:], reqId: 0, caller: self)
+    }
+    let networkManager:Networking = Networking()
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+    }
+    
+    
+    func setDictResponse(resp: NSDictionary, reqId: Int) {
+        
+    }
+    
+    func setArrayResponse(resp: NSArray, reqId: Int) {
+        
+        if
+        
+        
+        for Donor in resp {
+            //print(Donor)
+            print( Donor.valueForKey("email")!)
+            print( Donor.valueForKey("password")!)
+            
+            
+            
+            //            let d = doctor as! NSDictionary
+            //            let phone:String = d.objectForKey("civilId") as! String
+            //            let email:String = d.objectForKey("email") as! String
+            //            let name:String = "\(d.objectForKey("firstName")!) \(d.objectForKey("middleName")!) \(d.objectForKey("lastName")!)"
+            
+          //  let d = doctor as! NSDictionary
+            // let phone:String = d.objectForKey("phone") as! String
+             //let email:String = d.objectForKey("email") as! String
+            //let name:String = "\(d.objectForKey("name")!)"
+          //  addFriend(name, phone: phone, email: email)
+            
+        }
 
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
