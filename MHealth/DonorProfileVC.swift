ca37c8484822783a57062d9b8fc2bc68fc2887c6
RageQ8 <mhd-10@hotmail.com>
2017-03-07 09:32:38 +0300

    Update profile

--- MHealth/DonorProfileVC.swift
+++ MHealth/DonorProfileVC.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class DonorProfileVC: UIViewController {
+class DonorProfileVC: UIViewController,NetworkCaller {
     
     @IBOutlet var Logout: UIButton!
     @IBAction func Logout(sender: AnyObject) {
@@ -19,12 +19,16 @@ class DonorProfileVC: UIViewController {
         
         
     }
-    
-    
-    
     @IBOutlet var NameField: UITextField!
     @IBOutlet var LNameField: UITextField!
     @IBOutlet var EmailField: UITextField!
+    @IBOutlet var passwordField: UITextField!
+    @IBOutlet var Phone: UITextField!
+    @IBOutlet var BloodTypeField: UITextField!
+    @IBOutlet var NationalityField: UITextField!
+    @IBOutlet var BirthDate: UITextField!    
+    
+    
     @IBAction func EditUpdate(sender: AnyObject) {
         let s:UIButton = sender as! UIButton
         
@@ -43,6 +47,21 @@ class DonorProfileVC: UIViewController {
             
             // update profile with database
             
+            var n:Networking = Networking()
+            var dit = [String: AnyObject]()
+            dit["civilId"] = donor.civilID
+            dit["firstName"] = NameField.text
+            dit["lastName"] = LNameField.text
+            dit["password"] = passwordField.text
+            dit["nationality"] = NationalityField.text
+            dit["email"] = EmailField.text
+            dit["phoneNumber"] = Phone.text
+            dit["gender"] = donor.gender
+            dit["bloodType"] = BloodTypeField.text
+            dit["birthDate"] = "2017-01-01T00:00:00Z"
+            
+
+            n.AMJSONDictionary("http://34.196.107.188:8080/mHealthWS/ws/donor/" + "\(donor.donorID)", httpMethod: "PUT", jsonData: dit, reqId: 0, caller: self)
             
             
             NameField.userInteractionEnabled = false
@@ -57,12 +76,25 @@ class DonorProfileVC: UIViewController {
         }
         
     }
+    func setDictResponse(resp: NSDictionary, reqId: Int) {
+        for Donor in resp {
+            print(Donor)
+            
+        }
+    }
+    
+    func setArrayResponse(resp: NSArray, reqId: Int) {
+        
+        
+        
+        
+        for Donor in resp {
+            print(Donor)
+            
+        }
+        
+    }
 
-    @IBOutlet var passwordField: UITextField!
-    @IBOutlet var Phone: UITextField!
-    @IBOutlet var BloodTypeField: UITextField!
-    @IBOutlet var NationalityField: UITextField!
-    @IBOutlet var BirthDate: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         NameField.text = donor.firstName
