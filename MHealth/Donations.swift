14b14704ec465b3389e42638a7c10aff31fce584
iOS <trn24@trn24s-MacBook-Pro.local>
2017-04-10 11:18:45 +0300

    Donations

---
+++ MHealth/Donations.swift
@@ -0,0 +1,30 @@
+//
+//  Donations.swift
+//  MHealth
+//
+//  Created by trn24 on 4/10/17.
+//  Copyright © 2017 PIFSS. All rights reserved.
+//
+
+import UIKit
+
+class Donations: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+
+}
