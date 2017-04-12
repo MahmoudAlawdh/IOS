494d1066506f6b1cfea552f4b2fd8411c0955b52
iOS <trn15@trn15s-MacBook-Pro.local>
2017-04-12 11:17:51 +0300

    up

--- MHealth/EntryViewController.swift
+++ MHealth/EntryViewController.swift
@@ -22,13 +22,13 @@ class EntryViewController: VideoSplashViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let url = NSURL.fileURLWithPath(NSBundle.mainBundle().pathForResource("trans", ofType: "mp4")!)
+        let url = NSURL.fileURLWithPath(NSBundle.mainBundle().pathForResource("transtion2", ofType: "mp4")!)
         self.videoFrame = view.frame
         self.fillMode = .ResizeAspectFill
         self.alwaysRepeat = true
         self.sound = true
         self.startTime = 0.0
-        self.duration = 10.0
+        self.duration = 13.0
         self.alpha = 1.0
         self.backgroundColor = UIColor.clearColor()
         self.contentURL = url
